be90edaf0030a4dec40fbc16dbaccd6ed7f0512f
Raghed Idris <raghed.edriss@isae.edu.lb>
2019-03-21 10:41:54 +0300

    fix: question2

--- tp2/question2/FahrenheitCelsius.java
+++ tp2/question2/FahrenheitCelsius.java
@@ -15,18 +15,19 @@ public class FahrenheitCelsius{
       *  @param args ...
       */
      public static void main(String[] args){
-       try{
-            for (int i=0;i<args.length;i++){
-               int fahrenheit = Integer.parseInt(args[i]);
-                float celsius = fahrenheitEnCelsius(fahrenheit);
-            System.out.println(fahrenheit + "\u00B0F -> " + celsius + "\u00B0C"); // ligne,
-           }                                                       // format
-                 
-      
-       }catch(NumberFormatException nfe){
-           System.out.println("error : " + nfe.getMessage());  // en cas d'erreur 
-       }
-       
+        for (int i=0;i<args.length;i++){
+               try{
+                  
+                       int fahrenheit = Integer.parseInt(args[i]);
+                        float celsius = fahrenheitEnCelsius(fahrenheit);
+                    System.out.println(fahrenheit + "\u00B0F -> " + celsius + "\u00B0C"); // ligne,
+                                                                        // format
+                         
+              
+               }catch(NumberFormatException nfe){
+                   System.out.println("error : " + nfe.getMessage());  // en cas d'erreur 
+               }
+       }  
      }
      
      /** 
